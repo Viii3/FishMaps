0cc5a77965f24e8e763697abf28a73a315ff7803
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2024-08-02 14:17:35 +0100

    Remove centering from servlet.

--- src/main/java/fish/payara/fishmaps/world/VisualMapServlet.java
+++ src/main/java/fish/payara/fishmaps/world/VisualMapServlet.java
@@ -1,7 +1,6 @@
 package fish.payara.fishmaps.world;
 
 import fish.payara.fishmaps.world.block.Block;
-import fish.payara.fishmaps.world.block.Chunk;
 import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -27,20 +26,18 @@ public class VisualMapServlet extends HttpServlet {
         int height = Integer.parseInt(req.getParameter("height"));
         String dimension = req.getParameter("dimension");
 
-        int leftMost = blockX - (width / 2);
-        int topMost = blockZ - (height / 2);
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int x = 0; x < width; ++x) {
             for (int z = 0; z < height; ++z) {
-                Block block = this.service.getBlock(x + leftMost, z + topMost, dimension);
+                Block block = this.service.getBlock(x + blockX, z + blockZ, dimension);
                 if (block != null) image.setRGB(x, z, block.getColour());
                 else image.setRGB(x, z, 0);
             }
         }
 
-        resp.setContentType("image/png");
+        resp.setContentType("image/bmp");
         OutputStream stream = resp.getOutputStream();
-        ImageIO.write(image, "png", stream);
+        ImageIO.write(image, "bmp", stream);
         stream.close();
     }
 }
