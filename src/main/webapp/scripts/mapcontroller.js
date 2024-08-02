1a12d86dae15f1e733f532f1ea8816b9bd6f1332
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2024-08-02 14:19:18 +0100

    Add centering and large step sizes to the javascript.

--- src/main/webapp/scripts/mapcontroller.js
+++ src/main/webapp/scripts/mapcontroller.js
@@ -1,33 +1,45 @@
-const WIDTH = 1080;
-const HEIGHT = 720;
+const WIDTH = 1920;
+const HEIGHT = 1080;
+const STEP = 40;
 
 var currentX = 0;
 var currentZ = 0;
+var dimension = "minecraft:overworld";
 
 function renderMap () {
     let map = document.getElementById("mapView");
     map.innerHTML = "";
-    let image = document.createElement("img");
-    image.src = "images/map?x=" + currentX + "&z=" + currentZ + "&dimension=minecraft:overworld&width=" + WIDTH + "&height=" + HEIGHT;
-    map.appendChild(image);
+
+    const topLeftX = Math.floor(currentX - (WIDTH / 2));
+    const topLeftZ = Math.floor(currentZ - (HEIGHT / 2));
+    for (let z = 0; z < HEIGHT / STEP; ++z) {
+        for (let x = 0; x < WIDTH / STEP; ++x) {
+            let image = document.createElement("img");
+            image.class = "mapTile";
+            image.style = "width:" + STEP + "px;height:" + STEP + "px;"
+            image.src = "images/map?x=" + (topLeftX + currentX + x * STEP) + "&z=" + (topLeftZ + currentZ + z * STEP) + "&dimension=" + dimension + "&width=" + STEP + "&height=" + STEP;
+            map.appendChild(image);
+        }
+        map.appendChild(document.createElement("br"));
+    }
 }
 
 function up () {
-    currentZ++;
+    currentZ += STEP;
     renderMap();
 }
 
 function down () {
-    currentZ--;
+    currentZ -= STEP;
     renderMap();
 }
 
 function left () {
-    currentX--;
+    currentX += STEP;
     renderMap();
 }
 
 function right () {
-    currentX++;
+    currentX -= STEP;
     renderMap();
 }
