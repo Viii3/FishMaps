1171ec418948d0939e092dbc44f43ed0d54e81c4
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2024-08-12 16:22:25 +0100

    Add named queries for Player.

--- src/main/java/fish/payara/fishmaps/player/Player.java
+++ src/main/java/fish/payara/fishmaps/player/Player.java
@@ -2,13 +2,28 @@ package fish.payara.fishmaps.player;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
 
 @Entity
 @Table(name = "Players")
+@NamedQueries({
+    @NamedQuery(
+        name = Player.QUERY_COUNT,
+        query = "SELECT COUNT(p) FROM Player p"
+    ),
+    @NamedQuery(
+        name = Player.QUERY_POSITION,
+        query = "SELECT p FROM Player p INNER JOIN Block b ON p.location = b.descriptor WHERE b.dimension = :dimension AND b.x >= :minX AND b.x <= :maxX AND b.z >= :minZ AND b.z <= :maxZ"
+    )
+})
 public class Player implements Serializable {
+    public static final String QUERY_COUNT = "Player.count";
+    public static final String QUERY_POSITION = "Player.position";
+
     @Id
     private String name;
     private String location;
